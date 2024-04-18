a0e5530cdcb293baa655d842cdd16a7781878947
Festa <s317640@studenti.polito.it>
2024-04-18 17:31:09 +0200

    add MLP

--- models/FinalClassifier.py
+++ models/FinalClassifier.py
@@ -1,5 +1,5 @@
 from torch import nn
-
+import torch
 
 class Classifier(nn.Module):
     def __init__(self, num_classes):
@@ -10,3 +10,23 @@ class Classifier(nn.Module):
 
     def forward(self, x):
         return self.classifier(x), {}
+
+class MLP(nn.Module):
+    def __init__(self, num_input, num_classes, num_clips) -> None:
+        super().__init__()
+        self.num_clips = num_clips
+        self.num_input = num_input
+        self.classifier = nn.Sequential(
+            nn.Linear(self.num_input, 512),
+            nn.ReLU(),
+            nn.Linear(512,512),
+            nn.ReLU(),
+            nn.Linear(512, num_classes),
+            nn.Dropout(p=0.6)
+        )
+
+    def forward(self, x):
+        logits = []
+        for clip in range(self.num_clips):
+            logits.append(self.classifier(x[clip,:]))
+        return torch.stack(logits, dim=0).mean(dim=0), {}
