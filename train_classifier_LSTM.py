e0cd8689e5ed8a66846aceede9bb5d82bb2c0b15
FestaShabani <135214394+FestaShabani@users.noreply.github.com>
2024-04-20 17:45:00 +0200

    changed line 180 and commented out some stuff in validate(..)

--- train_classifier_LSTM.py
+++ train_classifier_LSTM.py
@@ -177,7 +177,7 @@ def train(action_classifier, train_loader, val_loader, device, num_classes):
         # in case of multi-clip training one clip per time is processed
         for m in modalities:
             data[m] = source_data[m].to(device)
-            data[m] = data[m] = torch.reshape(data[m], (args.models[m].num_clips, 32, 1024)) 
+            #data[m] = data[m] = torch.reshape(data[m], (args.models[m].num_clips, 32, 1024)) #this has to be changed according to late fusion or not 
 
         logits, _ = action_classifier.forward(data)
         action_classifier.compute_loss(logits, source_label, loss_weight=1)
@@ -234,18 +234,19 @@ def validate(model, val_loader, device, it, num_classes):
             for m in modalities:
                 batch = data[m].shape[0]
                 logits[m] = torch.zeros((args.test.num_clips, batch, num_classes)).to(device)
+                data[m].to(device) #added this
+                
+            #clip = {}
+            #for i_c in range(args.test.num_clips):
+                #for m in modalities:
+                    #clip[m] = data[m][:, i_c].to(device)
 
-            clip = {}
-            for i_c in range(args.test.num_clips):
-                for m in modalities:
-                    clip[m] = data[m][:, i_c].to(device)
-
-                output, _ = model(clip)
-                for m in modalities:
-                    logits[m][i_c] = output[m]
-
+            output, _ = model(clip)
             for m in modalities:
-                logits[m] = torch.mean(logits[m], dim=0)
+                logits[m][i_c] = output[m]
+
+            #for m in modalities:
+                #logits[m] = torch.mean(logits[m], dim=0)
 
             model.compute_accuracy(logits, label)
 
