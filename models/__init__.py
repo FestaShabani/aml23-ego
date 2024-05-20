0c520cea7681d03f589552112a52d64940854645
Festa <s317640@studenti.polito.it>
2024-05-20 18:04:45 +0200

    import models

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,5 @@ from models.VideoModel import VideoModel
 from models.I3D import I3D
 from models.FinalClassifier import Classifier, MLP
 from models.actionLSTM import ActionLSTM 
+from models.trn_mod import TRN
+from models.EMG import LeNet5
