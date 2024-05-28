f4e240b97264b2fed2dcb0ec2951f9da02d099fa
Festa <s317640@studenti.polito.it>
2024-05-28 11:04:11 +0200

    removed

--- save_feat_emg.py
+++ save_feat_emg.py
@@ -3,7 +3,6 @@ from utils.logger import logger
 import torch.nn.parallel
 import torch.optim
 import torch
-from utils.loaders import ActionNetDataset
 from utils.args import args
 from utils.utils import pformat_dict
 import utils
