66a5a385a675484b38439d3aa1ff116abe9f5473
Festa <s317640@studenti.polito.it>
2024-06-08 16:16:18 +0200

    TRN

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 from models.VideoModel import VideoModel
 from models.I3D import I3D
-from models.FinalClassifier import Classifier, MLP
+from models.FinalClassifier import Classifier, MLP, TRN
 from models.actionLSTM import ActionLSTM 
 from models.trn_mod import TRN
 from models.EMG import LeNet5
