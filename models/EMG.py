614df7adcbdb1c8d7f001d634751a512ce125700
Festa <s317640@studenti.polito.it>
2024-06-14 20:11:52 +0200

    mistaken name

--- models/EMG.py
+++ models/EMG.py
@@ -3,11 +3,11 @@ import torch
 import torch.nn.functional as F
 import numpy as np
 
-class LeNet5(nn.Module):
+class CNN(nn.Module):
 
     # network structure
     def __init__(self,num_classes=20):
-        super(LeNet5, self).__init__()
+        super(CNN, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=2, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(in_channels=2, out_channels=4, kernel_size=3, padding=1)
         self.fc1 = nn.Linear(1664, 832)  # Adjusted based on input size
--- models/__init__.py
+++ models/__init__.py
@@ -3,5 +3,5 @@ from models.I3D import I3D
 from models.FinalClassifier import Classifier, MLP, TRN
 from models.actionLSTM import ActionLSTM 
 from models.trn_mod import TRN_mod
-from models.EMG import LeNet5
+from models.EMG import CNN
 from models.VAE import VariationalAutoencoder
